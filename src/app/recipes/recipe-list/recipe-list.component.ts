a6b8cb974a8a0db0183336cdb86caf04ebaf7d8b
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-11-14 17:28:14 -0400

    Output dummy recipe data to recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from "@angular/core";
 
+import { Recipe } from "../recipe.model";
+
 @Component({
   selector: "app-recipe-list",
   templateUrl: "./recipe-list.component.html",
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: [];
+  recipes: Recipe[] = [
+    new Recipe(
+      "Hamburger",
+      "Recipe for a hamburguer",
+      "http://www.magimix.com/files/rec_22546/in_uk_hamburger.jpg"
+    )
+  ];
 
   constructor() {}
 
